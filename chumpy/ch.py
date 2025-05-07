15dae3b4f5f29d5f452bd528edd042a21e7cf82f
Colin Wilson <colin@cogsci.jhu.edu>
2025-05-07 11:36:07 -0400

    inspect (getargspec -> getfullargspec)

--- chumpy/ch.py
+++ chumpy/ch.py
@@ -1319,7 +1319,7 @@ def depends_on(*dependencies):
             [deps.add(d) for d in dep]
 
     def _depends_on(func):
-        want_out = 'out' in inspect.getargs(func).args
+        want_out = 'out' in inspect.getfullargspec(func).args
 
         @wraps(func)
         def with_caching(self, *args, **kwargs):
@@ -1365,7 +1365,7 @@ class ChLambda(Ch):
     def __init__(self, lmb, initial_args=None):
         args = {
             argname: ChHandle(x=Ch(idx))
-            for idx, argname in enumerate(inspect.getargs(lmb)[0])
+            for idx, argname in enumerate(inspect.getfullargspec(lmb)[0])
         }
         if initial_args is not None:
             for initial_arg in initial_args:
